a23b80c1f156c209ff2176982242952bc5f4ad84
Shamir Castro <shamir.castrocordova.9@hotmail.com>
2026-02-25 09:44:49 -0500

    login

---
+++ src/app/login/login.ts
@@ -0,0 +1,24 @@
+import { ChangeDetectionStrategy,Component ,signal} from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {FormControl, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MatSlideToggleModule,MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+    readonly email = new FormControl('', [Validators.required, Validators.email]);
+
+}
